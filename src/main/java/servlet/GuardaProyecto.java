fe1e57a297530b6b21288edf1d07db057d7b512a
Santiago Arevalo Gomez <santiagoarego@unisabana.edu.co>
2019-04-01 09:28:23 -0500

    sa

--- src/main/java/servlet/GuardaProyecto.java
+++ src/main/java/servlet/GuardaProyecto.java
@@ -23,9 +23,29 @@ public class GuardaProyecto extends HttpServlet{
     public void doPost(HttpServletRequest req, HttpServletResponse res)
     throws ServletException, IOException {
         Arreglos client = Arreglos.constructora();
+
+        if(!this.existeProyecto(req.getParameter("codigoProyecto"),client.getProyectos()){
         Proyecto pro = new Proyecto(req.getParameter("codigoProyecto"),
             req.getParameter("nombreProyecto"),Integer.parseInt(req.getParameter("duracionProyecto")),req.getParameter("plataformasDesarrollo"));
         client.guardaProyecto(pro);
+        pw.println("<HTML><SCRIPT>alert(\"Proyecto registrado\"); window.history.back();</SCRIPT></HTML>");
+        pw.close();
+        }else{
+            pw.println("<HTML><SCRIPT>alert(\"Codigo ya asignado\"); window.history.back();</SCRIPT></HTML>");
+            pw.close();
+        }
+    }
+    public boolean existeProyecto(String codigo, ArrayList<Proyecto> emp){
+        boolean existe=false;
+        for(int i=0; i<emp.size();i++) {
+            if(emp.get(i).getCodigo().equals(codigo)){
+                existe=true;
+                break;
+            }
+            
+        }
+        return existe;
+        
     }
 
 }
