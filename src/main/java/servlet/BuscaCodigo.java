5ddf79b76c5d55bde1368e48c352215d48648462
Santiago Arevalo Gomez <santiagoarego@unisabana.edu.co>
2019-04-01 09:17:51 -0500

    Commit

--- src/main/java/servlet/BuscaCodigo.java
+++ src/main/java/servlet/BuscaCodigo.java
@@ -18,18 +18,24 @@ public class BuscaCodigo extends HttpServlet{
     
 
 
-    
+    //Uso Dopost porque mis method en html es post, si usan get pues usen doGet, la estructura es la misma
 
     public void doPost(HttpServletRequest req, HttpServletResponse res)
     throws ServletException, IOException {
+
+        //Creo un arreglos, mi arreglos es el singleton
         Arreglos client = Arreglos.constructora();
+        //Traigo todos los proyectos del singleton
         ArrayList <Proyecto> pro= client.getProyectos();
+        //Meto el codigo que quiero buscar en una variable
         String cod = req.getParameter("buscaCodigo");
+        //Vainas que encontre en stackOverFlow jaja asies
         res.setContentType("text/html");
+        //Sirven para printear un HTML como respuesta
         PrintWriter pw = res.getWriter();
         pw.println("<HTML><HEAD><TITLE>Busca Proyecto</TITLE></HEAD>");
 
-        
+        //Pues el ciclo que me busca coincidencia
         for(int i=0; i<pro.size();i++){
             if(pro.get(i).getCodigo().equals(cod)){
                 
@@ -37,6 +43,7 @@ public class BuscaCodigo extends HttpServlet{
                 +"<br>Plataformas: "+pro.get(i).getPlataformas());
             }
         }
+        //Hay que decirle a la maquina cuando acabar jaja
         pw.close();
 
     }
--- src/main/java/servlet/GuardaEmpleado.java
+++ src/main/java/servlet/GuardaEmpleado.java
@@ -19,13 +19,34 @@ public class GuardaEmpleado extends HttpServlet{
 
     public void doPost  (HttpServletRequest req, HttpServletResponse res)
     throws ServletException, IOException {
-
+        res.setContentType("text/html");
+        //Sirven para printear un HTML como respuesta
+        PrintWriter pw = res.getWriter();
+        
         Arreglos sing = Arreglos.constructora();
+        if(!this.existeEmpleado(req.getParameter("documentoEmpleado"),sing.getEmpleados())){
+            Empleado emp = new Empleado(req.getParameter("documentoEmpleado"),
+                req.getParameter("nombreEmpleado"),req.getParameter("apellidoEmpleado"),req.getParameter("cargoEmpleado"),
+                req.getParameter("habilidadesEmpleado"));
+            sing.guardaEmpleado(emp);
+        }else{
+            pw.println("<HTML><SCRIPT>alert(\"no existe\");</SCRIPT></HTML>");
+            pw.close();
+        }
 
-        Empleado emp = new Empleado(req.getParameter("documentoEmpleado"),
-            req.getParameter("nombreEmpleado"),req.getParameter("apellidoEmpleado"),req.getParameter("cargoEmpleado"),
-            req.getParameter("habilidadesEmpleado"));
-        sing.guardaEmpleado(emp);
+    }
+
+    public boolean existeEmpleado(String documento, ArrayList<Empleado> emp){
+        boolean existe=false;
+        for(int i=0; i<emp.size();i++) {
+            if(emp.get(i).getDocumento.equals(documento)){
+                existe=true;
+                break;
+            }
+            
+        }
+        return existe;
+        
     }
 
 }
