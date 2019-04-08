273ba721af465e2adb9776ce2221f392478d0fa7
Santiago Arevalo Gomez <santiagoarego@unisabana.edu.co>
2019-04-08 09:00:21 -0500

    sa

--- src/main/java/servlet/GuardaVenta.java
+++ src/main/java/servlet/GuardaVenta.java
@@ -25,7 +25,7 @@ public class GuardaVenta extends HttpServlet {
         Arreglos sing = Arreglos.constructora();
         if (this.existeVendedor(req.getParameter("email"), sing.getVendedores(),req.getParameter("password"))) {
             Venta vent;
-            vent = new Venta(req.getParameter("correoVendedor"), req.getParameter("nombreProducto"), req.getParameter("lugarProcedencia")
+            vent = new Venta(req.getParameter("email"), req.getParameter("nombreProducto"), req.getParameter("lugarProcedencia")
                     , req.getParameter("nombreVendedor"), req.getParameter("descripcionProducto"), Integer.parseInt(req.getParameter("unidadesDisponibles")),
                     req.getParameter("fecha"), Integer.parseInt(req.getParameter("precioUnidad")));
             sing.guardaVenta(vent);
