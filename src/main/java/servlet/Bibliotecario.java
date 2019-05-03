246e7867e66dd7443b790e5c8b7c0e5fc7ebe023
yeissonpigo <48213359+yeissonpigo@users.noreply.github.com>
2019-05-03 12:22:33 -0500

    Iniciando la estructura del objeto.

--- src/main/java/servlet/Bibliotecario.java
+++ src/main/java/servlet/Bibliotecario.java
@@ -1,1 +1,12 @@
+package Servlet;
 
+public class Bibliotecario extends Usuario 
+{
+  private String dependencia;
+
+  public class Bibliotecario(String nombre, String correo, String contraseña, int id, String dependencia)
+  { 
+    super(nombre, correo, contraseña, id);
+    this.dependencia = dependencia;
+  }
+}
