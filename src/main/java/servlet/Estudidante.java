741471132b9e08798590ed48d59eef8d1683996f
yeissonpigo <yeissonpigo@unisabana.edu.co>
2019-05-05 21:33:37 -0500

    Creación del usuario tipo estudiante

---
+++ src/main/java/servlet/Estudidante.java
@@ -0,0 +1,22 @@
+package servlet;
+
+public class Estudiante extends Usuario
+{
+  private String facultad;
+
+  public  Estudiante(String nombre, String correo, String contraseña, int id, String facultad)
+  {
+    super(nombre, correo, contraseña, id);
+    this.facultad = facultad;
+  }
+
+  public String getFacultad()
+  {
+    return this.facultad;
+  }
+
+  public void setFacultad(String name)
+  {
+    this.facultad=name;
+  }
+}
