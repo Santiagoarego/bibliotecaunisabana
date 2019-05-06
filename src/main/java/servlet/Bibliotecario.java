af443a1432b1b34d97047e160bea85135d7301ee
yeissonpigo <yeissonpigo@unisabana.edu.co>
2019-05-05 21:32:11 -0500

    Agregué métodos en bibliotecario

--- src/main/java/servlet/Bibliotecario.java
+++ src/main/java/servlet/Bibliotecario.java
@@ -1,12 +1,22 @@
 package servlet;
 
-public class Bibliotecario extends Usuario 
+public class Bibliotecario extends Usuario
 {
   private String dependencia;
 
   public  Bibliotecario(String nombre, String correo, String contraseña, int id, String dependencia)
-  { 
+  {
     super(nombre, correo, contraseña, id);
     this.dependencia = dependencia;
+  }
+
+  public String getDependencia()
+  {
+    return this.dependencia;
+  }
+
+  public void setDependencia(String name)
+  {
+    this.dependencia=name;
   }
 }
